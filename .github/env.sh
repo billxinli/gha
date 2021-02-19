784aeb63452dd46fa6894a631d69fbb75920ab87
Bill Li <me@billxinli.com>
2021-02-19 13:46:03 -0500

    wip

---
+++ .github/env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "API_VERSION=123" >> "${GITHUB_ENV}"
--- .github/fail.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "Hello"
-node -e "process.exit(1)"
-echo "After hello"
